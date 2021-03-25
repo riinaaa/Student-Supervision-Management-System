0d61b667ba530329f32d22eea10a0306faf804f6
riinaaa <78783436+riinaaa@users.noreply.github.com>
2021-03-25 11:37:52 +0300

    still working on it

--- src/frontEnd/login.java
+++ src/frontEnd/login.java
@@ -94,63 +94,84 @@ public class login {
      * @return checkPass check KAU if true user can enter if false user will not be able to enter the system
      */
     
-    public boolean isCorrectPasswordSTD(String password){
-        this.password=password;
-        boolean checkPass;
-        System.out.println(password.charAt(0));
-        checkPass = password.charAt(0) =='A' && password.charAt(1)=='a';
-            return checkPass;
-    }
-
-    /**
-     *not every person can add books to the system. only employees thus, this method checks if the entered password by the employees
-    after checking their ID is correct according to the data stored in the file
-     * @param password
-     * @param ID
-     * @return check if the password of the employee corect
-     * @throws FileNotFoundException
-     */
-   
-    public static boolean isCorrectPasswordEmp(String password, String ID) throws FileNotFoundException{
-      
-   boolean check = false;
-   //employee ID then password stored in the file
-           File output = new File("Employee.txt");
-           Scanner read = new Scanner(output);
-      while(read.hasNext()){
-          switch(read.next()){
-              case "1" : 
-                 String info [] = read.next().split(",");
-                 if(password.equals(info[2])){
-            check=true;
-                 }
-            break;
-              case "2": 
-                 String info2 [] =read.next().split(",");
-                 if(password.equals(info2[2])){
-            check=true; }break;
-              case "3":
-                      String info3 [] = read.next().split(",");
-                 if(password.equals(info3[2])){
-            check=true;}
-              break;
-              case "4":
-                      String info4 [] = read.next().split(",");
-                 if(password.equals(info4[2])){
-            check=true;}
-            break;
-            case "5" : 
-                              String info5 [] = read.next().split(",");
-                 if(password.equals(info5[2])){
-            check=true;
-                      break;
-      }
     
-    }
- 
-    }
-       return check;
-    }
+    public static int AuthLogin(String fileID, String pass) {
+		try {
+			Scanner input = new Scanner(new File("AccountsInfo.txt"));
+			while (input.hasNext()) {
+				int ID = input.nextInt();
+				String User_Name = input.next();
+				String Password = input.next();
+				if (User_Name.equalsIgnoreCase(fileID) && Password.equals(pass)) {
+					return ID;
+				}
+			}
+			return -1;
+		} catch (FileNotFoundException ex) {
+			System.out.println("Error:File not found");
+			return -1;
+		}
+	}
+    
+    
+    
+//    public boolean isCorrectPasswordSTD(String password){
+//        this.password=password;
+//        boolean checkPass;
+//        System.out.println(password.charAt(0));
+//        checkPass = password.charAt(0) =='A' && password.charAt(1)=='a';
+//            return checkPass;
+//    }
+//
+//    /**
+//     *not every person can add books to the system. only employees thus, this method checks if the entered password by the employees
+//    after checking their ID is correct according to the data stored in the file
+//     * @param password
+//     * @param ID
+//     * @return check if the password of the employee corect
+//     * @throws FileNotFoundException
+//     */
+//   
+//    public static boolean isCorrectPasswordEmp(String password, String ID) throws FileNotFoundException{
+//      
+//   boolean check = false;
+//   //employee ID then password stored in the file
+//           File output = new File("Employee.txt");
+//           Scanner read = new Scanner(output);
+//      while(read.hasNext()){
+//          switch(read.next()){
+//              case "1" : 
+//                 String info [] = read.next().split(",");
+//                 if(password.equals(info[2])){
+//            check=true;
+//                 }
+//            break;
+//              case "2": 
+//                 String info2 [] =read.next().split(",");
+//                 if(password.equals(info2[2])){
+//            check=true; }break;
+//              case "3":
+//                      String info3 [] = read.next().split(",");
+//                 if(password.equals(info3[2])){
+//            check=true;}
+//              break;
+//              case "4":
+//                      String info4 [] = read.next().split(",");
+//                 if(password.equals(info4[2])){
+//            check=true;}
+//            break;
+//            case "5" : 
+//                              String info5 [] = read.next().split(",");
+//                 if(password.equals(info5[2])){
+//            check=true;
+//                      break;
+//      }
+//    
+//    }
+// 
+//    }
+//       return check;
+//    }
      
     /**
      *this method checks if the ID entered by the user is correct to access the system and be able to add books to the system
